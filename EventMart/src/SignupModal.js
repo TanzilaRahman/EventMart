c29a2319691e193d4318f08cfd812b626144708e
CheliMex <92612257+CheliMex@users.noreply.github.com>
2024-12-12 00:53:45 -0500

    Update SignupModal.js

--- EventMart/src/SignupModal.js
+++ EventMart/src/SignupModal.js
@@ -3,12 +3,14 @@ import Modal from "react-modal";
 import { createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from "./firebase"; // Import the Firebase auth instance
 import './SignupModal.css'; // Import CSS file
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 function SignupModal({ isOpen, onClose, setUser }) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [error, setError] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleSignup = async (e) => {
         e.preventDefault();
@@ -24,19 +26,6 @@ function SignupModal({ isOpen, onClose, setUser }) {
             onClose(); // Close the modal on success
         } catch (err) {
             setError("Failed to login with email/password.");
-            console.error(err.message);
-        }
-    };
-
-    const handleGoogleLogin = async () => {
-        try {
-            const result = await signInWithPopup(auth, googleProvider);
-            const user = result.user;
-            setUser(user); // Update the user state with the logged-in user
-            // alert("Login successful!");
-            onClose(); // Close modal after successful login
-        } catch (err) {
-            setError("Google login failed.");
             console.error(err.message);
         }
     };
@@ -61,31 +50,29 @@ function SignupModal({ isOpen, onClose, setUser }) {
                         required
                     />
                     <input
-                        type="password"
-                        placeholder="Password"
+                        type={showPassword ? "text" : "password"} // Toggle between text and password
+                        placeholder="Create Password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         className="input-field"
                         required
                     />
                     <input
-                        type="password"
+                        type={showPassword ? "text" : "password"}
                         placeholder="Confirm Password"
                         value={confirmPassword}
                         onChange={(e) => setConfirmPassword(e.target.value)}
                         className="input-field"
                         required
                     />
+                    <span className="show-password-btn-signup" onClick={() => setShowPassword(!showPassword)}>
+                            {showPassword ? <FaEyeSlash/> : <FaEye/>}
+                        </span>
                     <button type="submit" className="signup-button"> {/* Change button class */}
                         Sign Up
                     </button>
                 </form>
-                {/* Google Login Button */}
-                <div className="google-login">
-                    <button onClick={handleGoogleLogin} className="google-btn">
-                        Signup With Google
-                    </button>
-                </div>
+
                 <button onClick={onClose} className="cancel-button">
                     Cancel
                 </button>
